332cd3edaed93155fded209079b1de44b2fa80c4
faizdotid <faizalnurpratama76@gmail.com>
2024-08-19 01:14:52 +0700

    feat: implement matcher from matcher package

--- pkg/core/scanner.go
+++ pkg/core/scanner.go
@@ -2,25 +2,27 @@ package core
 
 import (
 	"fmt"
+	"go-exposed-config-scanner/pkg/color"
+	"go-exposed-config-scanner/pkg/matcher"
 	"go-exposed-config-scanner/pkg/utils"
-	"go-exposed-config-scanner/pkg/validators"
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 type Scanner struct {
 	c         *http.Client
-	v         validators.ValidatorFunction
+	m         matcher.Matcher
 	o         *os.File
 	name      string
 	matchFrom string
 }
 
-func NewScanner(client *http.Client, validator validators.ValidatorFunction, output *os.File, name string, matchFrom string) *Scanner {
+func NewScanner(client *http.Client, m matcher.Matcher, output *os.File, name string, matchFrom string) *Scanner {
 	return &Scanner{
 		c:         client,
-		v:         validator,
+		m:         m,
 		o:         output,
 		name:      name,
 		matchFrom: matchFrom,
@@ -47,25 +49,36 @@ func (s *Scanner) Scan(url string, count uint64, totalCount uint64) {
 		s.logError(count, totalCount, err)
 		return
 	}
-
-	valid, err := s.v(content)
-	if err != nil {
-		s.logError(count, totalCount, err)
-		return
-	}
-
-	if valid {
-		utils.WriteFile(s.o, []byte(url))
-		s.logResult(count, totalCount, "OK", url)
-	} else {
-		s.logResult(count, totalCount, "FAIL", url)
-	}
+	s.printResult(url, count, totalCount, resp.StatusCode, content)
 }
 
 func (s *Scanner) logError(count uint64, totalCount uint64, err error) {
-	fmt.Printf("[ %d / %d ] %s - ERR - %s\n", count, totalCount, s.name, err.Error())
+	fmt.Printf("[ %d / %d ] - [ %s ] - %s\n", count, totalCount, color.Red.AnsiFormat("ERROR"), err)
 }
 
-func (s *Scanner) logResult(count uint64, totalCount uint64, status, url string) {
-	fmt.Printf("[ %d / %d ] %s - %s - %s\n", count, totalCount, s.name, status, url)
+func (s *Scanner) printResult(url string, count, totalCount uint64, statusCode int, content []byte) {
+	statusStr := strconv.Itoa(statusCode)
+	var statusColor color.Color
+
+	switch {
+	case statusCode >= 200 && statusCode < 300:
+		statusColor = color.BoldGreen
+		if s.m.Match(content) {
+			fmt.Printf("[ %d / %d ] - [ %s ] - %s\n", count, totalCount, color.Green.AnsiFormat(s.name), url)
+			utils.WriteFile(s.o, []byte(url))
+			return
+		} else {
+			statusColor = color.BoldYellow
+		}
+	case statusCode >= 300 && statusCode < 400:
+		statusColor = color.BoldCyan
+	case statusCode >= 400 && statusCode < 500:
+		statusColor = color.BoldRed
+	case statusCode >= 500:
+		statusColor = color.BoldMagenta
+	default:
+		statusColor = color.White
+	}
+
+	fmt.Printf("[ %d / %d ] - [ %s ] - %s\n", count, totalCount, statusColor.AnsiFormat(statusStr), url)
 }
